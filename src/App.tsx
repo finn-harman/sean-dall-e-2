4172defb0f8278d22c82e684d27fe92402cdd54c
Finn Harman <finnharman@finns-mbp.home>
2023-01-30 20:55:26 +0000

    added logic + images

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import sean from './seanProfilePicture.jpg';
 import './App.css';
 
+const MAX_PHOTO_COUNT = 17;
+const PHOTOS_PER_PROMPT = 12;
+
 function App() {
+  const commonPath = process.env.PUBLIC_URL + "/images";
+
+  const [photoCount, setPhotoCount] = useState(0);
+  const [path, setPath] = useState(commonPath + "/Muybridge_horse_gallop_animated_2.gif");
+  
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * (max)) + 1;
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      let photoPath;
+      if (photoCount === 0) {
+        photoPath = "/Muybridge_horse_gallop_animated_2.gif";
+      } else if (photoCount === MAX_PHOTO_COUNT + 1) {
+        setPhotoCount(0);
+        photoPath = "/Muybridge_horse_gallop_animated_2.gif"
+      } else {
+        photoPath = `/prompts/${photoCount}_${getRandomInt(PHOTOS_PER_PROMPT)}.png`;
+      }
+      setPath(commonPath + photoPath);
+    }, photoCount === 1 ? 300 : 0)
+  }, [photoCount]);
+
+  console.log("path", path);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={sean} className="App-logo" alt="logo" />
-        <p>
-          {`Hello Sean :)`}
-        </p>
+        <img src={path} onClick={() => setPhotoCount(photoCount + 1)} />
       </header>
     </div>
   );
