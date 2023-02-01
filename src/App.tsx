f8c2121a2ad260b55141e6ec48b98854983619b3
Finn Harman <finnharman@finns-mbp.home>
2023-02-01 22:55:45 +0000

    Final changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-const MAX_PHOTO_COUNT = 17;
+const MAX_PHOTO_COUNT = 15;
 const PHOTOS_PER_PROMPT = 12;
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      let photoPath;
+      // setTimeout((
+      let photoPath : string;
       if (photoCount === 0) {
         photoPath = "/Muybridge_horse_gallop_animated_2.gif";
       } else if (photoCount === MAX_PHOTO_COUNT + 1) {
@@ -25,15 +25,19 @@ function App() {
       } else {
         photoPath = `/prompts/${photoCount}_${getRandomInt(PHOTOS_PER_PROMPT)}.png`;
       }
-      setPath(commonPath + photoPath);
-    }, photoCount === 1 ? 300 : 0)
+      if (photoCount === 1) {
+        setPath("")
+      };
+      setTimeout(() => {
+        setPath(commonPath + photoPath)
+      }, photoCount === 1 ? 300 : 0)
   }, [photoCount]);
 
   console.log("path", path);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={path} onClick={() => setPhotoCount(photoCount + 1)} style={{cursor: "pointer"}} />
+        <img src={path} onClick={() => setPhotoCount(photoCount + 1)} style={{cursor: "pointer", maxWidth: "85%"}} />
       </header>
     </div>
   );
